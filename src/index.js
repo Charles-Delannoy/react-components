b8ed0bc48fe2c5b8b08c691a2623f9e5ecf04cd6
Charles Delannoy <charles.delannoy@hei.fr>
2020-09-08 13:38:03 +0200

    my first component

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import CommentDetail from './CommentDetail';
+
+const App = () => {
+  return (
+    <div className="ui container comments">
+      <CommentDetail name="Sam" text="Hi there!"/>
+      <CommentDetail name="Jane" text="Let dive into React"/>
+      <CommentDetail name="Jon" text="Great"/>
+    </div>
+  );
+};
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App />,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
